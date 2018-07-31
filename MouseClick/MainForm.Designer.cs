7ad649644e98a67f120d0308ceea15f83e65361d
LightSmileMu <bluetears888@126.com>
2018-07-31 15:14:06 +0800

    添加项目

---
+++ MouseClick/MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace MouseClick
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTimeInterval = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbY = new MouseClick.WatermarkTextBox();
+            this.tbX = new MouseClick.WatermarkTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tbY);
+            this.groupBox1.Controls.Add(this.tbX);
+            this.groupBox1.Controls.Add(this.tbTimeInterval);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 133);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "鼠标设置";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(188, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "s";
+            // 
+            // tbTimeInterval
+            // 
+            this.tbTimeInterval.Location = new System.Drawing.Point(69, 96);
+            this.tbTimeInterval.Name = "tbTimeInterval";
+            this.tbTimeInterval.Size = new System.Drawing.Size(113, 21);
+            this.tbTimeInterval.TabIndex = 4;
+            this.tbTimeInterval.Text = "3";
+            this.tbTimeInterval.TextChanged += new System.EventHandler(this.tbTimeInterval_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "点击间隔:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X:";
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(142, 164);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(98, 23);
+            this.btnClick.TabIndex = 5;
+            this.btnClick.Text = "开始点击(F9)";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(69, 63);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(113, 21);
+            this.tbY.TabIndex = 3;
+            this.tbY.WatermarkText = "按下F8自动获取";
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(69, 28);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(113, 21);
+            this.tbX.TabIndex = 2;
+            this.tbX.WatermarkText = "按下F8自动获取";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 199);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "鼠标点击器";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private WatermarkTextBox tbY;
+        private WatermarkTextBox tbX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbTimeInterval;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MouseClick/MainForm.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MouseClick
+{
+    public partial class MainForm : Form
+    {
+        private readonly KeyboardHook _kHook = new KeyboardHook();
+        private KeyboardHook.KeyboardHookCallback _myKeyEventHandeler; //按键钩子
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartListen()
+        {
+            try
+            {
+                _myKeyEventHandeler = hook_KeyDown;
+                _kHook.KeyDown += _myKeyEventHandeler; //钩住键按下
+                _kHook.Install(); //安装键盘钩子
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void StopListen()
+        {
+            if (_myKeyEventHandeler != null)
+            {
+                _kHook.KeyDown -= _myKeyEventHandeler; //取消按键事件
+                _myKeyEventHandeler = null;
+                _kHook.Install(); //关闭键盘钩子
+            }
+        }
+
+        private void hook_KeyDown(KeyboardHook.VKeys key)
+        {
+            if (key == KeyboardHook.VKeys.F8)
+            {
+                tbX.Text = MousePosition.X.ToString();
+                tbY.Text = MousePosition.Y.ToString();
+            }
+            else if (key == KeyboardHook.VKeys.F9)
+            {
+                button1_Click(btnClick,null);
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            
+            StartListen();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            try
+            {
+                StopListen();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            base.OnClosing(e);
+        }
+
+        private void DoMouseClick()
+        {
+            int x;
+            int y;
+
+            if (int.TryParse(tbX.Text, out x))
+            {
+                if (int.TryParse(tbY.Text, out y))
+                {
+                    if (x >= 0 && y >= 0)
+                    {
+                        var dx = (int) ((double) x / Screen.PrimaryScreen.Bounds.Width *
+                                        65535); //屏幕分辨率映射到0~65535(0xffff,即16位)之间
+                        var dy = (int) ((double) y / Screen.PrimaryScreen.Bounds.Height *
+                                        0xffff); //转换为double类型运算，否则值为0、1
+                        mouse_event(MouseEventFlag.Move | MouseEventFlag.LeftDown | MouseEventFlag.LeftUp |
+                            MouseEventFlag.Absolute, dx, dy, 0, new UIntPtr(0)); //点击
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        ///     鼠标事件
+        /// </summary>
+        /// <param name="flags">事件类型</param>
+        /// <param name="dx">x坐标值(0~65535)</param>
+        /// <param name="dy">y坐标值(0~65535)</param>
+        /// <param name="data">滚动值(120一个单位)</param>
+        /// <param name="extraInfo">不支持</param>
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(MouseEventFlag flags, int dx, int dy, uint data, UIntPtr extraInfo);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null)
+            {
+                return;
+            }
+
+            timer1.Enabled = !timer1.Enabled;
+            if (button.Text == @"开始点击(F9)")
+            {
+                button.Text = @"停止点击(F9)";
+            }
+            else
+            {
+                button.Text = @"开始点击(F9)";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DoMouseClick();
+        }
+
+        private void tbTimeInterval_TextChanged(object sender, EventArgs e)
+        {
+            var box = sender as TextBox;
+            if (box == null)
+            {
+                return;
+            }
+            int interval;
+            if (int.TryParse(box.Text, out interval))
+            {
+                if (interval > 0)
+                {
+                    timer1.Interval = interval * 1000;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    ///     鼠标操作标志位集合
+    /// </summary>
+    [Flags]
+    internal enum MouseEventFlag : uint
+    {
+        /// <summary>
+        ///     鼠标移动事件
+        /// </summary>
+        Move = 0x0001,
+
+        /// <summary>
+        ///     鼠标左键按下事件
+        /// </summary>
+        LeftDown = 0x0002,
+        LeftUp = 0x0004,
+        RightDown = 0x0008,
+        RightUp = 0x0010,
+        MiddleDown = 0x0020,
+        MiddleUp = 0x0040,
+        XDown = 0x0080,
+        XUp = 0x0100,
+        Wheel = 0x0800,
+        VirtualDesk = 0x4000,
+
+        /// <summary>
+        ///     设置鼠标坐标为绝对位置（dx,dy）,否则为距离最后一次事件触发的相对位置
+        /// </summary>
+        Absolute = 0x8000
+    }
+}
